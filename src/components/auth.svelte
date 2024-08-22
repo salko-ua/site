77aa2181d580f98028e14165e0a48d73a558cb10
salko-ua <chips69jamil69game@gmail.com>
2024-08-23 00:33:39 +0300

    fix cut and added shadow

--- src/components/auth.svelte
+++ src/components/auth.svelte
@@ -7,107 +7,102 @@
 
 {#if (is_used === "login")}
 	<div class="login">
-	<div class="login-left-side">
-		<div class="greeting-mobile">
-			{$_('login.login-text')}
-		</div>
-	</div>
-	<div class="login-right-side">
-		<div class="contents">
-			<div class="form-login">
-				<div class="greeting-pc">
-					{$_('login.login-text')}
-				</div>
-				<div class="input-container ic2">
-					<input id="email" class="input" type="text" placeholder=" " />
-					<div class="cut">{$_('login.email')}</div>
-					<label for="email" class="placeholder">{$_('login.email')}</label>
-				</div>
-				<div class="input-container ic2">
-					<input id="password" class="input" type="password" placeholder=" " />
-					<div class="cut ">{$_('login.password')}</div>
-					<label for="password" class="placeholder">{$_('login.password')}</label>
-				</div>
-				<div class="checkbox-block">
-					<div class="checkbox"><Checkbox/></div>
-					<div class="checkbox-text">
-						{$_('login.checkbox-password')}
-					</div>
-					<div class="have-account">
-						<a href="/registration">{$_('login.have-account')}</a>
-					</div>
-				</div>
-				<button type="button" class="submit">{$_('login.accept')}</button>
-			</div>
-		</div>
-	</div>
-</div>
-{:else if (is_used === "registration")}
-	<div class="registration">
-		<div class="registration-left-side">
+		<div class="login-left-side">
 			<div class="greeting-mobile">
-				{$_('registration.registration-text')}
+				{$_('login.login-text')}
 			</div>
 		</div>
-		<div class="registration-right-side">
+		<div class="login-right-side">
 			<div class="contents">
-				<div class="form-registration">
+				<div class="form-login">
 					<div class="greeting-pc">
-						{$_('registration.registration-text')}
+						{$_('login.login-text')}
 					</div>
-					<div class="together-box">
-						<div class="input-container ic2 together" id="together-1">
-						<input
-							id="first-name"
-							class="input"
-							type="text"
-							placeholder=" "
-							required
-							minlength="2"
-							maxlength="16"
-						/>
-						<div class="cut">{$_('registration.first-name')}</div>
-						<label for="first_name" class="placeholder">{$_('registration.first-name')}</label>
-					</div>
-						<div class="input-container ic2 together" id="together-2">
-						<input
-							id="last-name"
-							class="input"
-							type="text"
-							placeholder=" "
-							minlength="2"
-							maxlength="16"
-						/>
-						<div class="cut"> {$_('registration.last-name')} </div>
-						<label for="last-name" class="placeholder"> {$_('registration.last-name')} </label>
-					</div>
-					</div>
-
 					<div class="input-container ic2">
 						<input id="email" class="input" type="text" placeholder=" " />
-						<div class="cut">{$_('registration.email')}</div>
-						<label for="email" class="placeholder">{$_('registration.email')}</label>
+						<label for="email" class="placeholder">{$_('login.email')}</label>
 					</div>
 					<div class="input-container ic2">
 						<input id="password" class="input" type="password" placeholder=" " />
-						<div class="cut ">{$_('registration.password')}</div>
-						<label for="password" class="placeholder">{$_('registration.password')}</label>
+						<label for="password" class="placeholder">{$_('login.password')}</label>
 					</div>
 					<div class="checkbox-block">
 						<div class="checkbox"><Checkbox/></div>
 						<div class="checkbox-text">
-							{$_('registration.checkbox-password')}
+							{$_('login.checkbox-password')}
 						</div>
 						<div class="have-account">
-							<a href="/login">{$_('registration.have-account')}</a>
+							<a href="/registration">{$_('login.have-account')}</a>
 						</div>
 					</div>
-					<button type="button" class="submit">{$_('registration.accept')}</button>
+					<button type="button" class="submit">{$_('login.accept')}</button>
 				</div>
 			</div>
 		</div>
 	</div>
+{:else if (is_used === "registration")}
+	<div class="registration">
+			<div class="registration-left-side">
+				<div class="greeting-mobile">
+					{$_('registration.registration-text')}
+				</div>
+			</div>
+			<div class="registration-right-side">
+				<div class="contents">
+					<div class="form-registration">
+						<div class="greeting-pc">
+							{$_('registration.registration-text')}
+						</div>
+						<div class="together-box">
+							<div class="input-container ic2 together" id="together-1">
+							<input
+								id="first-name"
+								class="input"
+								type="text"
+								placeholder=" "
+								required
+								minlength="2"
+								maxlength="16"
+							/>
+							<label for="first_name" class="placeholder">{$_('registration.first-name')}</label>
+						</div>
+							<div class="input-container ic2 together" id="together-2">
+							<input
+								id="last-name"
+								class="input"
+								type="text"
+								placeholder=" "
+								minlength="2"
+								maxlength="16"
+							/>
+							<label for="last-name" class="placeholder"> {$_('registration.last-name')} </label>
+						</div>
+						</div>
+
+						<div class="input-container ic2">
+							<input id="email" class="input" type="text" placeholder=" " />
+							<label for="email" class="placeholder">{$_('registration.email')}</label>
+						</div>
+						<div class="input-container ic2">
+							<input id="password" class="input" type="password" placeholder=" " />
+							<label for="password" class="placeholder">{$_('registration.password')}</label>
+						</div>
+						<div class="checkbox-block">
+							<div class="checkbox"><Checkbox/></div>
+							<div class="checkbox-text">
+								{$_('registration.checkbox-password')}
+							</div>
+							<div class="have-account">
+								<a href="/login">{$_('registration.have-account')}</a>
+							</div>
+						</div>
+						<button type="button" class="submit">{$_('registration.accept')}</button>
+					</div>
+				</div>
+			</div>
+		</div>
 {/if}
+
 
 <style>
 	* {
@@ -138,18 +133,19 @@
 		background-color: #1e1e2e;
 		position: absolute;
       	width: 100%;
-        height: min-content;
-		justify-content: center;
+		height: 100vh;
+        align-content: center;
+		margin: auto 0;
     }
 
 	/* Animation */
-	.input:focus ~ .cut,
-	.input:not(:placeholder-shown) ~ .cut {
-	  	transform: translateY(8px);
-	}
 	.input:focus ~ .placeholder,
 	.input:not(:placeholder-shown) ~ .placeholder {
-	  	transform: translateY(-30px) translateX(10px) scale(0.75);
+	  	transform: translateY(-38px) translateX(2px) scale(0.75);
+		background-color: #181825;
+		padding: 8px 10px;
+		border-radius: 10px;
+		box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 	}
 	.input:not(:placeholder-shown) ~ .placeholder {
 	  	color: #808097;
@@ -194,21 +190,10 @@
 		padding: 4px 20px 0;
 		width: 100%;
 		float: left;
+		box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 	}
-	.cut {
-		color: #181825;
-        background-color: #181825;
-		border-radius: 10px;
-		height: 20px;
-		font-size: 12px;
-		left: 20px;
-		padding-left: 10px;
-		position: absolute;
-		top: -20px;
-		transform: translateY(0);
-		transition: transform 200ms;
-		width: max-content;
-
+	.submit {
+		box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 	}
 	.contents {
 		align-content: center;
@@ -230,6 +215,7 @@
 			margin: 5% auto 5% auto;
 			background-color: #181825;
 			border-radius: 30px;
+			box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 		}
 		.input-container {
 			height: 50px;
@@ -266,14 +252,15 @@
     @media screen and (max-width: 861px) {
 		.login-left-side, .registration-left-side {
 			width: 80%;
-			height: 10%;
-			padding: 20px 5%;
+			height: 50px;
+			padding: 10px 5%;
 			margin-left: 5%;
 			margin-top: 10%;
 			background-color: #11111b;
 			text-align: center;
 			align-content: center;
 			border-radius: 20px 20px 0 0;
+			box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 		}
 		.login-right-side, .registration-right-side {
 			width: 90%;
@@ -282,6 +269,7 @@
 			background-color: #181825;
 			border-radius: 0 0 20px 20px;
 			margin-bottom: 10%;
+			box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
 		}
 		.input-container {
 			height: 50px;
@@ -329,7 +317,6 @@
 			margin-top: 20px;
 			margin-bottom: 20px;
 		}
-
     }
 
 
